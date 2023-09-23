2763f272f85201318b3b0b0de3b7defdc535cb93
izmystic <mystic@izmystic.dev>
2023-09-22 22:34:19 -0400

    add: custom styling
    - yellow
    - duck
    - stolen header from overextended docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,68 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
-const config: DocsThemeConfig = {
-  logo: <span>Qbox</span>,
-  project: {
-    link: 'https://github.com/Qbox-project',
-  },
-  chat: {
-    link: 'https://discord.gg/qbox',
-  },
-  docsRepositoryBase: 'https://github.com/Qbox-project/qbox-docs',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+// https://github.com/overextended/overextended.github.io/blob/main/theme.config.tsx#L6-L37
+function useHead() {
+  const { asPath } = useRouter();
+  const { frontMatter, title } = useConfig();
+  const url = `https://qbox-docs.vercel.app${asPath}`;
+  const description = frontMatter.description || "Documentation for the Qbox Project";
+
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" type="image/x-icon" href="/qbox-duck.ico" />
+      <meta httpEquiv="Content-Language" content="en" />
+      <meta name="description" content={description} />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
+      <meta name="og:url" content={url} />
+    </>
+  );
 }
 
-export default config
+function useNextSeoProps() {
+  const { asPath } = useRouter();
+  const arr = asPath.replace(/[-_]/g, " ").split("/");
+  const category = (arr[1][0] !== "#" && arr[1]) || "Qbox";
+  const rawTitle = arr[arr.length - 1];
+  const title = /[a-z]/.test(rawTitle) && /[A-Z]/.test(rawTitle) ? rawTitle : "%s";
+
+  return {
+    titleTemplate: `${title} - ${
+      rawTitle === category ? "Documentation" : category.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase())
+    }`,
+  };
+}
+
+const config: DocsThemeConfig = {
+  logo: (
+    <div
+      style={{
+        paddingLeft: "50px",
+        lineHeight: "38px",
+        background: "url(/qbox-duck.png) no-repeat left",
+        backgroundSize: "38px",
+        fontWeight: 550,
+      }}
+    >
+      Qbox
+    </div>
+  ),
+  project: {
+    link: "https://github.com/Qbox-project",
+  },
+  chat: {
+    link: "https://discord.gg/qbox",
+  },
+  docsRepositoryBase: "https://github.com/Qbox-project/qbox-docs",
+  footer: {
+    text: "Qbox Project",
+  },
+  primaryHue: { dark: 60, light: 60 },
+  head: useHead,
+  useNextSeoProps: useNextSeoProps,
+};
+
+export default config;
